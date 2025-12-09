b8a5e1997a7685a493240b85672899a8ed62b661
Vishwanath362 <156990926+Vishwanath362@users.noreply.github.com>
2025-12-09 23:21:13 +0530

    trying to redeploy

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -109,7 +109,7 @@ const Navbar = () => {
                   </svg>
                   <span className="text-sm">LinkedIn</span>
                 </a>
-                
+
                 <a
                   href="https://github.com/Vishwanath362"
                   target="_blank"
@@ -130,5 +130,6 @@ const Navbar = () => {
     </nav>
   )
 }
+
 
 export default Navbar
